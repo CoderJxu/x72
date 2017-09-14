d5b23fddce9dfc676d58d3c91e8d06212c23e792
Jianrui Xu <xujianrui@gmail.com>
2017-09-14 18:05:06 +1000

    fixing

--- x72alinux/x72qialivm.sh
+++ x72alinux/x72qialivm.sh
@@ -1,30 +1,66 @@
 #!/usr/bin/env bash
 
+# to dawnload this script
+# curl -L http://bit.ly/2x0Wons >x72alinstall.sh
+
+f_ptc(){
+  read -e -sn 1 -p "Press any key to continue..."
+}
+
 #----------------------------------------------------------
 # Preparation
 # confirm you can access the internet
-if [[ ! $(curl -Is http://www.google.com/ | head -n 1) =~ "200 OK" ]]; then
+connection_test() {
+  # `cmd` 执行命令cmd
+  # ip route 显示路由表
+  # grep 使用正则表达式搜索文本，并把匹配的行打印出来
+  # awk 一种编程语言，用于对文本和数据进行处理
+  #   ‘script’ 表示要执行的脚本script
+  #   ‘NR’ 表示记录数，在执行过程中对应于当前的行号
+  #   {print} 组行逐行扫描文件并重复执行print
+  #   $n 表示当前记录的第n个字段
+  # ping 用以测试网络的连通性
+  #   -q 不显示指令执行过程，开头和结尾的相关信息除外
+  #   -c <完成次数> 设置完成要求回应的次数
+  #   -w 设置等待应答时间
+  # &> file 命令执行后，输出和错误都定向到file中
+  ping -q -w 1 -c 1 `ip route | grep default | awk 'NR==1 {print $3}'` &> /dev/null && return 1 || return 0
+}
+
+if [[ ! connection_test  ]]; then
+  clear
   echo "Your Internet seems broken. Press Ctrl-C to abort or enter to continue."
   read
 fi
 
+clear
+echo "To make sure system clock is accurate"
 timedatectl set-ntp true
-
+timedatectl status
+f_ptc
 #----------------------------------------------------------
 ### Partitioning
 
+clear
+echo "Start Partitioning!"
 # make partitions on the disk.
 # MBR partition table
-parted -s /dev/sda mktable msdos
+parted -s /dev/sda mklabel gpt
 # /boot - 512m
-parted -s /dev/sda mkpart primary 0% 512m
+parted -s /dev/sda mkpart ESP fat32 1MiB 513MiB
+parted -s set 1 boot on
 # / (root) - 20g
-parted -s /dev/sda mkpart primary 512m 20992m
+parted -s /dev/sda mkpart primary 513MiB 20993MiB
 # swap - 2g
-parted -s /dev/sda mkpart primary linux-swap 20992m 22016m
+parted -s /dev/sda mkpart primary linux-swap 20993MiB 23089MiB
 # /home - the rest
-parted -s /dev/sda mkpart primary 22016m 100%
+parted -s /dev/sda mkpart primary 23089MiB 100%
 
+echo "Partition Finished!"
+f_ptc
+
+clear
+echo "Start Formatting"
 # make filesystems
 # /boot
 mkfs.fat -F32 /dev/sda1
@@ -36,12 +72,20 @@ swapon /dev/sda3
 # /home
 mkfs.ext4 /dev/sda4
 
+echo "Format Finished!"
+f_ptc
+
+clear
+echo "Start Mounting the partitions"
 # set up /mnt
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 mkdir /mnt/home
 mount /dev/sda4 /mnt/home
+
+echo "Mount Finished!"
+f_ptc
 
 #----------------------------------------------------------
 # rankmirrors to make this faster (though it takes a while)
