d561b61a9f04a086cdf279bcfaab47a0614a92bd
Jianrui Xu <xujianrui@gmail.com>
2017-09-16 21:41:03 +1000

    bug fix

--- x72alinux/x72qialivm.sh
+++ x72alinux/x72qialivm.sh
@@ -4,11 +4,26 @@
 # curl -L http://bit.ly/2x0Wons >x72alinstall.sh
 
 f_ptc(){
+  sleep 2
   read -e -sn 1 -p "Press any key to continue..."
 }
 
 #----------------------------------------------------------
 # Preparation
+
+#----------------------------------------------------------
+clear
+echo "Pre-installation - Verify the boot mode"
+if [[ -d "/sys/firmware/efi/"]]; then
+  echo "The system is booted in UEFI mode"
+else
+  echo "The system is booted in BIOS or CSM mode"
+fi
+f_ptc
+
+#----------------------------------------------------------
+clear
+echo "Pre-installation - Connect to the Internet"
 # confirm you can access the internet
 connection_test() {
   # `cmd` 执行命令cmd
@@ -28,124 +43,157 @@ connection_test() {
 }
 
 if [[ ! connection_test  ]]; then
-  clear
-  echo "Your Internet seems broken. Press Ctrl-C to abort or enter to continue."
-  read
+  echo "Internet seems broken. Press any key to abort."
+  read -e -sn 1
+  safeExit
+else
+  echo "Internet is connected."
+  f_ptc
 fi
 
+#----------------------------------------------------------
 clear
-echo "To make sure system clock is accurate"
+echo "Pre-installation - Update the system clock"
 timedatectl set-ntp true
 timedatectl status
 f_ptc
 
 #----------------------------------------------------------
-### Partitioning
-
 clear
-echo "Start Partitioning!"
-# make partitions on the disk.
-# MBR partition table
-parted /dev/sda mklabel msdos
+echo "Pre-installation - Partition the disks"
+# /(bootloader) - 2m
 # /boot - 512m
-parted /dev/sda mkpart primary ext3 1MiB 513MiB
-parted /dev/sda set 1 boot on
-# / (root) - 20g
-parted /dev/sda mkpart primary ext3 513MiB 20GiB
 # swap - 2g
-parted /dev/sda mkpart primary linux-swap 20GiB 22GiB
-# /home - the rest
-parted /dev/sda mkpart primary ext3 22GiB 100%
-
-echo "Partition Finished!"
-f_ptc
-
-clear
-echo "Start Formatting"
-# make filesystems
-# /boot
-mkfs.fat -F32 /dev/sda1
-# /
-mkfs.ext4 /dev/sda2
-# swap
-mkswap /dev/sda3
-swapon /dev/sda3
-# /home
-mkfs.ext4 /dev/sda4
-
-echo "Format Finished!"
-f_ptc
-
-clear
-echo "Start Mounting the partitions"
-# set up /mnt
-mount /dev/sda2 /mnt
-mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
-mkdir /mnt/home
-mount /dev/sda4 /mnt/home
-
-echo "Mount Finished!"
+# /(root) - the rest
+parted /dev/sda mklabel gpt
+parted /dev/sda mkpart primary 1MiB 3MiB
+parted /dev/sda set 1 bios_grub on
+parted /dev/sda mkpart primary ext3 3MiB 515MiB
+parted /dev/sda mkpart primary linux-swap 515MiB 2GiB
+parted /dev/sda mkpart primary ext3 2GiB 100%
 f_ptc
 
 #----------------------------------------------------------
-# rankmirrors to make this faster (though it takes a while)
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.orig
-rankmirrors -n 6 /etc/pacman.d/mirrorlist.orig >/etc/pacman.d/mirrorlist
-pacman -Syy
+clear
+echo "Pre-installation - Format the partitions"
+# make filesystems
+# /(bootloader) - 2m
+# /boot - 512m
+# swap - 2g
+# /(root) - the rest
+mkfs.vfat /dev/sda1
+mkfs.ext4 /dev/sda2
+mkswap /dev/sda3
+swapon /dev/sda3
+mkfs.ext4 /dev/sda4
+f_ptc
+
+#----------------------------------------------------------
+clear
+echo "Pre-installation - Mount the file systems"
+# set up /mnt
+mount /dev/sda4 /mnt
+mkdir -p /mnt/boot
+mount /dev/sda1 /mnt/boot
+f_ptc
 
 #----------------------------------------------------------
 ### Installation
-# install base packages (take a coffee break if you have slow internet)
-pacstrap /mnt base base-devel
+# rankmirrors to make this faster (though it takes a while)
+clear
+echo "Installation - Select the mirrors"
+mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.orig
+rankmirrors -n 6 /etc/pacman.d/mirrorlist.orig >/etc/pacman.d/mirrorlist
+pacman -Syy
+f_ptc
 
+#----------------------------------------------------------
+# install base packages (take a coffee break if you have slow internet)
+clear
+echo "Installation - Install the base packages"
+pacstrap /mnt base base-devel
+f_ptc
+
+#----------------------------------------------------------
+### Configure the system
 # generate fstab
+clear
+echo "Configure the system - Fstab"
 genfstab -U -p /mnt >>/mnt/etc/fstab
+cat /mnt/etc/fstab
+f_ptc
 
 # copy ranked mirrorlist over
 cp /etc/pacman.d/mirrorlist* /mnt/etc/pacman.d
 
 # chroot
+clear
+echo "Configure the system - chroot - Time zone"
 arch-chroot /mnt /bin/bash <<EOF
 # set initial timezone to Australia/Sydney
 ln -s /usr/share/zoneinfo/Australia/Sydney /etc/localtime
 # adjust the time skew, and set the time standard to UTC
 hwclock --systohc --utc
+EOF
 
+echo "Configure the system - chroot - Locale"
+arch-chroot /mnt /bin/bash <<EOF
 # set initial locale
-sed -i '/en_US.UTF-8/{s/#//}' /etc/locale.gen
-sed -i '/zh_CN.GB18030/{s/#//}' /etc/locale.gen
-sed -i '/zh_CN.GB2312/{s/#//}' /etc/locale.gen
-sed -i '/zh_CN.GBK/{s/#//}' /etc/locale.gen
-sed -i '/zh_CN.UTF-8/{s/#//}' /etc/locale.gen
-sed -i '/zh_TW.UTF-8/{s/#//}' /etc/locale.gen
+sed -i '/en_US/{s/#//}' /etc/locale.gen
+sed -i '/zh_CN/{s/#//}' /etc/locale.gen
+sed -i '/zh_TW/{s/#//}' /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
+EOF
 
+echo "Configure the system - chroot - Hostname"
+arch-chroot /mnt /bin/bash <<EOF
 # Network configuration
 # set initial hostname
 echo "x72al-$(date +"%Y%W")" >/etc/hostname
 echo "127.0.1.1\tx72al-$(date +"%Y%W").localdomain\tx72al-$(date +"%Y%W")" >> /etc/hosts
+EOF
 
+echo "Configure the system - chroot - Network configuration"
+arch-chroot /mnt /bin/bash <<EOF
 pacman -S networkmanager iw wpa_supplicant dialog
 systemctl enable NetworkManager.service
+EOF
 
+echo "Configure the system - chroot - Initramfs"
+arch-chroot /mnt /bin/bash <<EOF
 # no modifications to mkinitcpio.conf should be needed
 mkinitcpio -p linux
+EOF
 
+echo "Configure the system - chroot - Root password"
+arch-chroot /mnt /bin/bash <<EOF
+# set root password to "root"
+echo root:root | chpasswd
+EOF
+
+echo "Configure the system - chroot - intel-ucode"
+if [[ $(less /proc/cpuinfo | grep GenuineIntel | awk '{print $3}') = "GenuineIntel" ]]; then
+arch-chroot /mnt /bin/bash <<EOF
 pacman -S intel-ucode
+EOF
+fi
 
+echo "Configure the system - chroot - Boot loader"
+arch-chroot /mnt /bin/bash <<EOF
 pacman -S grub os-prober
 grub-install --target=i386-pc /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
-
-# set root password to "root"
-echo root:root | chpasswd
-
-# end section sent to chroot
 EOF
 
+echo "Configure the system - chroot - packages installation"
+arch-chroot /mnt /bin/bash <<EOF
+pacman -S screen screenfetch wpa_actiond ifplugd sudo zsh
+EOF
+
+# end section sent to chroot
+
 # unmount
-umount /mnt/{boot,}
+umount -R /mnt
 
 echo "Done! Unmount the CD image from the VM, then type 'reboot'."
